512d993af2e78b6686046620b6800714953532cf
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 03:11:18 +1030

    data types

---
+++ www/data_types.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			$phrase = "To be or not to be"; //string
+            $age = 30; //inteager
+            $gpa = 3.3; //float
+            $isMale = true; //boolean
+            //no value: null
+
+            echo "$phrase <br>";
+            echo "$age <br>";
+            echo "$gpa <br>";
+            echo "$isMale <br>";
+		?>
+	</body>
+</html>
