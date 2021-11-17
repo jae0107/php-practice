5e43c3c4ede75b00ea0e2ab73c04649ab8f8f1da
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 02:20:37 +1030

    php variable

---
+++ www/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			//init variables
+			$characterName = "John";
+			$characterAge = 35;
+
+			echo "There once was a man named $characterName <br>";
+			echo "He was $characterAge years old <br>";
+
+			//update name
+			$characterName = "Mike";
+			echo "He really liked the name $characterName <br>";
+			echo "But didn't like being $characterAge <br>";
+		?>
+	</body>
+</html>
