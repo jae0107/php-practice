a08ba133ffdfd7802f5032fed752bb1f3c50e368
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 02:06:39 +1030

    hello world

---
+++ www/helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			echo "<h1>Hello World</h1>";
+			echo "<hr></hr>";
+			echo "<p>This is my site</p>";
+		?>
+	</body>
+</html>
