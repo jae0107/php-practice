a4cd27e02e3ee3be9e8feaba5e39ad277dad4425
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 03:38:55 +1030

    numbers

---
+++ www/numbers.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			echo -40.123;
+            echo "<br>";
+
+            echo 5.7 * 9;
+            echo "<br>";
+
+            echo 10 % 3;
+            echo "<br>";
+
+            $num = 10;
+            echo $num++;
+            echo "<br>";
+            echo $num;
+            echo "<br>";
+            echo $num += 25;
+            echo "<br>";
+
+            echo abs(-100);
+            echo "<br>";
+
+            echo pow(2, 4);
+            echo "<br>";
+
+            echo sqrt(144);
+            echo "<br>";
+
+            echo max(2, 10);
+            echo "<br>";
+
+            echo min(2, 10);
+            echo "<br>";
+
+            echo round(3.2);
+            echo "<br>";
+            echo round(3.7);
+            echo "<br>";
+
+            echo ceil(3.3);
+            echo "<br>";
+            echo floor(3.9);
+		?>
+	</body>
+</html>
