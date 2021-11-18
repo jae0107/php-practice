911471e686b8e79e11dc7a9c22e4a20c3354cfdf
jae0107 <ghjgjh0107@gmail.com>
2021-11-18 15:56:09 +1030

    calculator

---
+++ www/calculator.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="calculator.php" method="get">
+            <input type="number" name="num1">
+            <br>
+            <input type="number" name="num2">
+            <input type="submit">
+        </form>
+
+        <?php if(empty($_GET['num1']) && empty($_GET['num2'])) : ?>
+            Answer: 0
+        <?php elseif(isset($_GET['num1']) && isset($_GET['num2'])): ?>
+            Answer: <?php echo $_GET["num1"] + $_GET["num2"]?>
+        <?php else: ?>
+            Answer: 0
+        <?php endif; ?>
+    </body>
+</html>
