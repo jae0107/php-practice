4c60ccab0ea5a4a5ad6506edf3e2318bf982a2f3
jae0107 <ghjgjh0107@gmail.com>
2021-11-19 18:23:41 +1030

    comparison

---
+++ www/comparison.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Comparison</title>
+    </head>
+    <body>
+        <?php 
+            function getMax($num1, $num2, $num3){
+                if ($num1 >= $num2 && $num1 >= $num3) {
+                    return $num1;
+                } elseif ($num2 >= $num1 && $num2 >= $num3) {
+                    return $num2;
+                } else {
+                    return $num3;
+                }
+            }
+
+            echo getMax(40, 9, 40);
+        ?>
+    </body>
+</html>
