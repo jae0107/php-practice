e83b3ddda56c648e5835d33a7f5c4cc7887ef869
jae0107 <ghjgjh0107@gmail.com>
2021-11-20 19:20:33 +1030

    while

---
+++ www/while.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php 
+            $index = 1;
+            echo "while lopp<br>";
+            while ($index <= 5) {
+
+                echo"<script>console.log('Debug Objects: " . $index . "' );</script>"; # write on console
+                echo "$index <br>";
+                $index++;
+            }
+
+            echo "<br>do-while lopp<br>";
+            $index = 6;
+            do {
+                echo "$index <br>";
+                $index++;
+            } while ($index <= 5);
+        ?>
+    </body>
+</html>
